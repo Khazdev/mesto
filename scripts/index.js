d825674e3bec8cfbd62e600e27b92358b39075d9
Khazdev <xazanoff17@gmail.com>
2023-07-10 20:33:48 +0300

    fix after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-console.log('скрипт подключен');
-const elements = document.querySelector('.elements');
 // Получить элементы
 const popup = document.querySelector('.popup');
 const profileEditButton = document.querySelector('.profile__edit-button');
@@ -8,21 +6,21 @@ const popupSaveButton = popup.querySelector('.popup__save-button');
 
 const profileName = document.querySelector('.profile__name');
 const profileBio = document.querySelector('.profile__bio');
-const popupProfileName = popup.querySelector('.popup__name');
-const popupProfileBio = popup.querySelector('.popup__bio');
+const popupProfileName = popup.querySelector('.popup__input_name');
+const popupProfileBio = popup.querySelector('.popup__input_bio');
 
-const likeToggle = function (event) {
-  if (event.srcElement.classList.contains('elements__like-button')) {
-    event.srcElement.classList.toggle('elements__like-button_active');
-  }
-};
+// const likeToggle = function (event) {
+//   if (event.srcElement.classList.contains('elements__like-button')) {
+//     event.srcElement.classList.toggle('elements__like-button_active');
+//   }
+// };
 
 const popupToggle = function () {
   popup.classList.toggle('popup_opened');
-  console.log('кликнули на кнопку');
 };
 
-const saveInputPopup = function () {
+const saveInputPopup = function (event) {
+  event.preventDefault();
   profileName.textContent = popupProfileName.value;
   profileBio.textContent = popupProfileBio.value;
   popupToggle();
@@ -41,7 +39,7 @@ popupCloseButton.addEventListener('click', popupToggle);
 // Навесить слушатель на клик по кнопке сохранить
 popupSaveButton.addEventListener('click', saveInputPopup);
 
-elements.addEventListener('click', likeToggle);
+// elements.addEventListener('click', likeToggle);
 
 
 
