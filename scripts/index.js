cd55502b56c621192ce0e2081a88e78bba6dc599
Khazdev <xazanoff17@gmail.com>
2023-07-12 20:41:15 +0300

    fix after review 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,21 @@
 // Получить элементы
-const popup = document.querySelector('.popup');
 const profileEditButton = document.querySelector('.profile__edit-button');
-const popupCloseButton = popup.querySelector('.popup__close');
-const popupSaveButton = popup.querySelector('.popup__save-button');
-
 const profileName = document.querySelector('.profile__name');
 const profileBio = document.querySelector('.profile__bio');
-const popupProfileName = popup.querySelector('.popup__input_name');
-const popupProfileBio = popup.querySelector('.popup__input_bio');
+// ПопАп и все что в нем
+const popup = document.querySelector('.popup');
+const popupCloseButton = popup.querySelector('.popup__close');
+const popupForm = popup.querySelector('.popup__form');
+const popupProfileName = popupForm.querySelector('.popup__input_text_name');
+const popupProfileBio = popupForm.querySelector('.popup__input_text_bio');
 
-// const likeToggle = function (event) {
-//   if (event.srcElement.classList.contains('elements__like-button')) {
-//     event.srcElement.classList.toggle('elements__like-button_active');
-//   }
-// };
 
 const popupToggle = function () {
   popup.classList.toggle('popup_opened');
 };
 
 const saveInputPopup = function (event) {
+  // console.log(popupForm);
   event.preventDefault();
   profileName.textContent = popupProfileName.value;
   profileBio.textContent = popupProfileBio.value;
@@ -36,12 +32,16 @@ const editProfile = function () {
 profileEditButton.addEventListener('click', editProfile);
 // Навесить слушатель на клик по крестику
 popupCloseButton.addEventListener('click', popupToggle);
-// Навесить слушатель на клик по кнопке сохранить
-popupSaveButton.addEventListener('click', saveInputPopup);
+// Навесить слушатель на submit формы
+popupForm.addEventListener('submit', saveInputPopup);
 
+// На будущее кнопка лайка
+// const likeToggle = function (event) {
+//   if (event.srcElement.classList.contains('elements__like-button')) {
+//     event.srcElement.classList.toggle('elements__like-button_active');
+//   }
+// };
 // elements.addEventListener('click', likeToggle);
-
-
 
 
 
